833a00a793f86a1ff8e9d8899f3b104456df3c99
Satya dev Reddy <tsatyadevreddy@gmail.com>
2015-12-03 16:13:36 +0530

    Changes and fix for cors untested

--- app/controllers/api/v1/physicians_controller.rb
+++ app/controllers/api/v1/physicians_controller.rb
@@ -9,13 +9,15 @@ module Api
 				# 	unprocessble_data
 				# 	return
 				# end
-
+				byebug
 				if params[:filters][:by] == 'all'
-					render json: PhysicianDetail.by_location_and_speciality(param_data[:filters][:location_id], param_data[:filters][:speciality_id]), content_type: Mime::JSON, status: 200
+					render json: Physician.by_location_and_speciality(params[:filters][:location_id], params[:filters][:speciality_id]), content_type: Mime::JSON, status: 200
 				elsif params[:filters] == 'involved'
-					render json: PhysicianDetail.by_all(param_data['location_id'], param_data['speciality_id']).by_involved, content_type: Mime::JSON, status: 200
+					render json: Physician.by_all(params['location_id'], params['speciality_id']).by_involved, content_type: Mime::JSON, status: 200
 				elsif params[:filters] == 'lead'
-					render json: PhysicianDetail.by_all(param_data['location_id'], param_data['speciality_id']).by_lead, content_type: Mime::JSON, status: 200
+					render json: Physician.by_all(params['location_id'], params['speciality_id']).by_lead, content_type: Mime::JSON, status: 200
+				else
+					unprocessble_data
 				end
 			end
 
