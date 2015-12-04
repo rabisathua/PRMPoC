a02b7970e370f7a20bc6b04ff9b99c7f0b1047e8
Satya dev Reddy <tsatyadevreddy@gmail.com>
2015-12-04 15:23:39 +0530

    Fixed a small bug in physicians controller

--- app/controllers/api/v1/physicians_controller.rb
+++ app/controllers/api/v1/physicians_controller.rb
@@ -5,9 +5,9 @@ module Api
 			def index
 				if params[:filters][:by] == 'all'
 					render json: Physician.by_location_and_speciality(params[:filters][:location_id], params[:filters][:speciality_id]), content_type: Mime::JSON, status: 200
-				elsif params[:filters] == 'involved'
+				elsif params[:filters][:by] == 'involved'
 					render json: Physician.by_location_and_speciality(params[:filters][:location_id], params[:filters][:speciality_id]).by_involved, content_type: Mime::JSON, status: 200
-				elsif params[:filters] == 'lead'
+				elsif params[:filters][:by] == 'lead'
 					render json: Physician.by_location_and_speciality(params[:filters][:location_id], params[:filters][:speciality_id]).by_lead, content_type: Mime::JSON, status: 200
 				else
 					unprocessble_data
