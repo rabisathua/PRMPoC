a6eb89482474add0a98d386cf91fe02acbb8f5f3
Satya dev Reddy <tsatyadevreddy@gmail.com>
2015-12-07 15:00:02 +0530

    Fix for a bug where sign_out was not happening

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,7 @@
-require 'securerandom'
-
 class User < ActiveRecord::Base
   # Include default devise modules.
   # :registerable, #recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
   devise :database_authenticatable, :trackable
   include DeviseTokenAuth::Concerns::User
-  before_validation :set_uid
-
-  private
-  def set_uid
-  	self.uid = SecureRandom.uuid.gsub('-', '')
-  end
+  
 end
